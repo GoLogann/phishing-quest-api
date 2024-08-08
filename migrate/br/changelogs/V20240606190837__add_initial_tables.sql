de0a25f4b1b6a175f80208dd1905914598dac7c7
Logan Cardoso <logan.cc@outlook.com>
2024-08-08 13:41:30 -0300

    feat: migrations para o banco de dados

--- migrate/br/changelogs/V20240606190837__add_initial_tables.sql
+++ migrate/br/changelogs/V20240606190837__add_initial_tables.sql
@@ -1,61 +1,53 @@
--- Criação do schema phishing_quest, se ainda não existir
 CREATE SCHEMA IF NOT EXISTS phishing_quest;
 
--- Criação da tabela users
 CREATE TABLE IF NOT EXISTS phishing_quest.users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    xp INT DEFAULT 0,
-    total_score INT DEFAULT 0
-    );
-
--- Criação da tabela categories
-CREATE TABLE IF NOT EXISTS phishing_quest.categories (
-    category_id SERIAL PRIMARY KEY,
-    category_name VARCHAR(255) NOT NULL UNIQUE
+                                                    id UUID PRIMARY KEY,
+                                                    username VARCHAR(255) NOT NULL UNIQUE,
+                                                    email VARCHAR(255) NOT NULL UNIQUE,
+                                                    password_hash VARCHAR(255) NOT NULL,
+                                                    total_score INT DEFAULT 0,
+                                                    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                                                    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
--- Criação da tabela questions
+CREATE TABLE IF NOT EXISTS phishing_quest.categories (
+                                                         id UUID PRIMARY KEY,
+                                                         category_name VARCHAR(255) NOT NULL UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS phishing_quest.questions (
-    question_id SERIAL PRIMARY KEY,
-    category_id INT NOT NULL REFERENCES phishing_quest.categories(category_id),
-    question_text TEXT NOT NULL,
-    correct_answer VARCHAR(255) NOT NULL
-    );
+                                                        id UUID PRIMARY KEY,
+                                                        category_id UUID NOT NULL REFERENCES phishing_quest.categories(id),
+                                                        question_text TEXT NOT NULL,
+                                                        correct_answer VARCHAR(255) NOT NULL
+);
 
--- Criação da tabela answers
 CREATE TABLE IF NOT EXISTS phishing_quest.answers (
-    answer_id SERIAL PRIMARY KEY,
-    question_id INT NOT NULL REFERENCES phishing_quest.questions(question_id),
-    answer_text VARCHAR(255) NOT NULL,
-    is_correct BOOLEAN NOT NULL
-    );
+                                                      id UUID PRIMARY KEY,
+                                                      question_id UUID NOT NULL REFERENCES phishing_quest.questions(id),
+                                                      answer_text VARCHAR(255) NOT NULL,
+                                                      is_correct BOOLEAN NOT NULL
+);
 
--- Criação da tabela user_answers
 CREATE TABLE IF NOT EXISTS phishing_quest.user_answers (
-    user_answer_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES phishing_quest.users(user_id),
-    question_id INT NOT NULL REFERENCES phishing_quest.questions(question_id),
-    answer_id INT NOT NULL REFERENCES phishing_quest.answers(answer_id),
-    is_correct BOOLEAN,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ai_rating INT CHECK (ai_rating BETWEEN 1 AND 5) DEFAULT NULL
-    );
+                                                           user_answer_id UUID PRIMARY KEY,
+                                                           user_id UUID NOT NULL REFERENCES phishing_quest.users(id),
+                                                           question_id UUID NOT NULL REFERENCES phishing_quest.questions(id),
+                                                           answer_id UUID NOT NULL REFERENCES phishing_quest.answers(id),
+                                                           is_correct BOOLEAN,
+                                                           timestamp TIMESTAMPTZ DEFAULT NOW(),
+                                                           ai_rating INT CHECK (ai_rating BETWEEN 1 AND 5) DEFAULT NULL
+);
 
--- Criação da tabela user_scores
 CREATE TABLE IF NOT EXISTS phishing_quest.user_scores (
-    user_score_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES phishing_quest.users(user_id),
-    score INT NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+                                                          id UUID PRIMARY KEY,
+                                                          user_id UUID NOT NULL REFERENCES phishing_quest.users(id),
+                                                          score INT NOT NULL,
+                                                          timestamp TIMESTAMPTZ DEFAULT NOW()
+);
 
--- Adicionando índices para otimização
 CREATE INDEX IF NOT EXISTS idx_user_answers_user_id ON phishing_quest.user_answers(user_id);
 CREATE INDEX IF NOT EXISTS idx_user_answers_question_id ON phishing_quest.user_answers(question_id);
 CREATE INDEX IF NOT EXISTS idx_user_answers_answer_id ON phishing_quest.user_answers(answer_id);
 
--- Adicionando comentário para clarificar o propósito da coluna ai_rating
 COMMENT ON COLUMN phishing_quest.user_answers.ai_rating IS 'Avaliação da IA de 1 a 5: 1-Péssimo, 2-Ruim, 3-Mediano, 4-Bom, 5-Excelente';
