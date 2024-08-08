9af27bfdfc45a518202f60d3ae1746844cfeae2a
Logan Cardoso <logan.cc@outlook.com>
2024-08-08 13:39:33 -0300

    feat: adicionando logs a conexão com o banco de dados

--- cmd/main.go
+++ cmd/main.go
@@ -6,13 +6,10 @@ import (
 )
 
 func main() {
-	// Inicializa o container de dependências
 	cont := container.NewContainer()
 
-	// Configura o router com os handlers do container
 	r := http.SetupRouter(cont)
 
-	// Inicia o servidor
 	err := r.Run(":8080")
 	if err != nil {
 		return
