f186caaa7ef00e0a0dc2c06c4f3d7af3850816ac
Logan Cardoso <logan.cc@outlook.com>
2024-08-08 17:30:48 -0300

    refactor: modificando path da API

--- adapter/http/router/user_router.go
+++ adapter/http/router/user_router.go
@@ -7,9 +7,9 @@ import (
 
 // SetupUserRoutes configura as rotas relacionadas a usuários
 func SetupUserRoutes(router *gin.Engine, userHandler *handler.UserHandler) {
-	userGroup := router.Group("/users")
+	userGroup := router.Group("/api/v1/users")
 	{
-		userGroup.POST("/", userHandler.CreateUser)
+		userGroup.POST("", userHandler.CreateUser)
 		userGroup.GET("/:id", userHandler.GetUser)
 		// Outras rotas relacionadas a usuários
 	}
