df37ef001f9d276ad24d6f16f0382b040f308229
Logan Cardoso <logan.cc@outlook.com>
2025-01-01 12:47:39 -0300

    feat(domain): add domain for Category

--- domain/category.go
+++ domain/category.go
@@ -1,1 +1,20 @@
 package domain
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+)
+
+type Category struct {
+	Id           uuid.UUID `json:"id" gorm:"primaryKey"`
+	CategoryName string    `json:"categoryName" validate:"required,min=1,max=255"`
+}
+
+func (c *Category) TableName() string {
+	return "phishing_quest.categories"
+}
+
+func (c *Category) Validate() error {
+	validate := validator.New()
+	return validate.Struct(c)
+}
