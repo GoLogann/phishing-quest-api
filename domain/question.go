e2b5bef3c1602a5ad61d1d4b6b6eeb93ed18f9d1
Logan Cardoso <logan.cc@outlook.com>
2025-01-01 12:47:20 -0300

    feat(domain): add domain for question

--- domain/question.go
+++ domain/question.go
@@ -1,1 +1,22 @@
 package domain
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+)
+
+type Question struct {
+	Id            uuid.UUID `json:"id" gorm:"primaryKey"`
+	CategoryId    uuid.UUID `json:"categoryId" validate:"required"`
+	QuestionText  string    `json:"questionText" validate:"required"`
+	CorrectAnswer string    `json:"correctAnswer" validate:"required"`
+}
+
+func (q *Question) TableName() string {
+	return "phishing_quest.questions"
+}
+
+func (q *Question) Validate() error {
+	validate := validator.New()
+	return validate.Struct(q)
+}
