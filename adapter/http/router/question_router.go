0e335895ff3a0a1273f12f0783684488ec6a642b
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-04 15:21:43 -0300

    feat(router): create routes for CRUD question

--- adapter/http/router/question_router.go
+++ adapter/http/router/question_router.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"phishing-quest/adapter/http/handler"
+)
+
+func SetupQuestionsRoutes(router *gin.Engine, questionsHandler *handler.QuestionHandler) {
+	categoryGroup := router.Group("api/v1/questions")
+	{
+		categoryGroup.POST("", questionsHandler.CreateQuestion)
+		categoryGroup.GET("/:id", questionsHandler.GetQuestion)
+		categoryGroup.PUT("/:id", questionsHandler.UpdateQuestion)
+		categoryGroup.DELETE("/:id", questionsHandler.DeleteQuestion)
+	}
+}
