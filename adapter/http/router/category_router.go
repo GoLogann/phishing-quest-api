755e33af13c5aaaaba1d91abacf9191c7a855acb
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-04 12:24:40 -0300

    feat(router): create  routes for list and create category

--- adapter/http/router/category_router.go
+++ adapter/http/router/category_router.go
@@ -2,12 +2,13 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"phishing-quest/adapter/http/handler"
 )
 
-func SetupCategoryRoutes(router *gin.Engine) {
-	categoryGroup := router.Group("/categories")
+func SetupCategoryRoutes(router *gin.Engine, categoryHandler *handler.CategoryHandler) {
+	categoryGroup := router.Group("api/v1/categories")
 	{
-		categoryGroup.POST("/")
-		categoryGroup.GET("/")
+		categoryGroup.POST("", categoryHandler.CreateCategory)
+		categoryGroup.GET("", categoryHandler.ListCategory)
 	}
 }
