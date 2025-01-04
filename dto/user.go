61bfde819fbbfbff2530d6ebb9222da9bb31ca4b
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-04 12:31:54 -0300

    feat(dto): create dto for user login response

--- dto/user.go
+++ dto/user.go
@@ -1,6 +1,15 @@
 package dto
 
+import "github.com/google/uuid"
+
 type UserLoginDTO struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type UserLoginResponseDTO struct {
+	Id         uuid.UUID `json:"id"`
+	Username   string    `json:"username"`
+	Email      string    `json:"email"`
+	TotalScore int       `json:"totalScore"`
 }
