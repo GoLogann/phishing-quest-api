b435995fbb3e765b78bd99c6b0bd16862d54f890
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-05 10:36:35 -0300

    feat(dto): create AnswerDTO and QuestionAnswersDTO

--- dto/answer.go
+++ dto/answer.go
@@ -1,1 +1,14 @@
 package dto
+
+import "github.com/google/uuid"
+
+type QuestionAnswersDTO struct {
+	QuestionId uuid.UUID    `json:"questionId"`
+	Answers    []*AnswerDTO `json:"answers"`
+}
+
+type AnswerDTO struct {
+	Id         uuid.UUID `json:"id"`
+	AnswerText string    `json:"answerText"`
+	IsCorrect  bool      `json:"isCorrect"`
+}
