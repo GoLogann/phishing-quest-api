1f4bd0ee72b03ed91b4c8620949c470fdf4cc855
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-10 18:17:13 -0300

    fix: remove coments

--- core/usecase/ranking_usecase.go
+++ core/usecase/ranking_usecase.go
@@ -9,12 +9,10 @@ type RankingUseCase struct {
 	rankingRepo repository.IRankingRepository
 }
 
-// NewRankingUseCase cria uma nova instância de RankingUseCase
 func NewRankingUseCase(rankingRepo repository.IRankingRepository) *RankingUseCase {
 	return &RankingUseCase{rankingRepo: rankingRepo}
 }
 
-// GetGlobalRanking retorna o ranking global, paginado
 func (ruc *RankingUseCase) GetGlobalRanking(limit, offset int) ([]domain.UserScore, error) {
 	ranking, err := ruc.rankingRepo.GetGlobalRanking(limit, offset)
 	if err != nil {
